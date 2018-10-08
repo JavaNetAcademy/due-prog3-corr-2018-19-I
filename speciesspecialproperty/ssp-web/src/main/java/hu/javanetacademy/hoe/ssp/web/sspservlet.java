7e62cd8b5e7e034e185747fc2c62bc5bc144ef8e
Szabó Viktor <user@i5>
2018-10-08 23:11:28 +0200

    ssp-web

---
+++ speciesspecialproperty/ssp-web/src/main/java/hu/javanetacademy/hoe/ssp/web/sspservlet.java
@@ -0,0 +1,32 @@
+package hu.javanetacademy.hoe.ssp.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author user
+ */
+@WebServlet(name = "sspservice", urlPatterns = { "/ssp/ssp" })
+public class sspservlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+}
