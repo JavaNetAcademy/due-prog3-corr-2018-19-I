4cc6940e32a050738fda6d62b0f98ff7b98b3294
Szabó Viktor <>
2018-10-08 19:55:52 +0200

    ssp

---
+++ specialspeciesproperty/ssp-dao-model/src/main/java/hu/javanetacademy/hoe/ssp/dao/model/ssp.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.javanetacademy.hoe.ssp.dao.model;
+
+/**
+ *
+ * @author user
+ */
+public class ssp {
+    
+    private long id;
+    private String name;
+    private String description;
+
+    public ssp() {
+    }
+
+    public ssp(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+    
+}
