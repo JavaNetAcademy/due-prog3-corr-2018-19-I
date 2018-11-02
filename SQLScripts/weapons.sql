e329bc3e419e227331350c9c1b23028bd0f19d61
Richard Páj <pajrichard@gmail.com>
2018-11-02 16:50:48 +0100

    Fegyverek SQL szkript

---
+++ SQLScripts/weapons.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `weapons` (
+ `id` int(20) NOT NULL AUTO_INCREMENT,
+ `name` varchar(255) COLLATE utf8_bin NOT NULL,
+ `description` text COLLATE utf8_bin,
+ `price` int(11) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Fegyverek'
+
+CREATE TABLE `weapon_properties` (
+ `weapon_id` int(20) NOT NULL,
+ `property_id` int(20) NOT NULL,
+ PRIMARY KEY (`weapon_id`,`property_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
