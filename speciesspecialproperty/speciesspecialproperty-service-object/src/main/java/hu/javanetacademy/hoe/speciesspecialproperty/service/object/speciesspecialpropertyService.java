92843f6be6aa31342dd6243cb185b3ebe60aaeb2
Szabó Viktor <szabo.viktor.work@gmail.com>
2019-01-20 12:36:05 +0100

    SpeciesSpecialProperty working on webservlet 00,2

---
+++ speciesspecialproperty/speciesspecialproperty-service-object/src/main/java/hu/javanetacademy/hoe/speciesspecialproperty/service/object/speciesspecialpropertyService.java
@@ -0,0 +1,45 @@
+package hu.javanetacademy.hoe.speciesspecialproperty.service.object;
+
+import hu.javanetacademy.hoe.base.util.CustomException;
+import hu.javanetacademy.hoe.ssp.dao.jdbc.SspJDBCDAOImpl;
+
+import hu.javanetacademy.hoe.speciesspecialproperty.dao.model.SpeciesSpecialProperty;
+
+import java.util.List;
+import hu.javanetacademy.hoe.speciesspecialproperty.dao.model.SpeciesSpecialPropertyDao;
+
+/**
+ * @author sviktor75 / Szabó Viktor / vts4gv
+ */
+public class speciesspecialpropertyService {
+    
+    private SpeciesSpecialPropertyDao dao = new SspJDBCDAOImpl();
+    
+    public SpeciesSpecialProperty create (SpeciesSpecialProperty pNewSsp){
+        if (dao.getByName(pNewSsp.getName()) != null){ //Ha létezik már ilyen nevű ssp az adott ID-jű fajnak, akkor 
+            throw new CustomException(); //hibát jelez,
+        }
+        else {
+            return dao.create(pNewSsp); //különben létrehozza.
+        }
+    }
+    
+    public SpeciesSpecialProperty delete(long pSspId){
+        return dao.delete(pSspId);
+    }
+    
+    public SpeciesSpecialProperty getById(long pSspId){
+        return dao.getById(pSspId);
+    }
+    
+    public List<SpeciesSpecialProperty> getBySpecies(long pSpeciesId){
+        return dao.getBySpecies(pSpeciesId);
+    }
+    /*
+    public PropertyLevelAttribute modify(long pSspId){
+        return dao.modify(pSspId);
+    }
+    */
+    
+    
+}
